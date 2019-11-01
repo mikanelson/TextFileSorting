4704db03d2d3e6bc073803d377a5908deb2e23dc
Mika Nelson <mika_nelson39@mymail.eku.edu>
2019-11-01 16:24:49 -0400

    Rewrote return for sh

--- Sort/dist/test.sh
+++ Sort/dist/test.sh
@@ -7,5 +7,8 @@
    java -jar Sort.jar descending SortedDescendingGenerated.txt
    diff SortedDescendingGenerated.txt SortedDescending.txt
    descending_result=$?
-   if [ ascending_result && descending_result ] then
+   if [ ascending_result && descending_result ]; then
       exit 0
+   else
+      exit 1
+   fi
